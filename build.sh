1218e8ebe3ba9c000e26b162462954ac3bc545c2
intrand <intrand@users.noreply.github.com>
2020-12-19 13:55:58 +0000

    clean up existing images when overwriting

--- build.sh
+++ build.sh
@@ -11,6 +11,9 @@ version="${1}";
 docker build --build-arg "version=${version}" -t ansible-base:${version} -f "${root}/Dockerfile" "${root}" || exit 1;
 docker build --build-arg "version=${version}" -t ansible:${version} -f "${root}/ansible.dockerfile" "${root}" || exit 1;
 for i in config connection console doc galaxy inventory playbook pull test vault; do
+	if [ $(docker image ls | grep -c "ansible-${i}:${version}") -gt 0 ]; then
+		docker image rm "ansible-${i}:${version}";
+	fi;
 	echo "Building ansible-${i}:${version}...";
 	docker build --build-arg "version=${version}" -t "ansible-${i}:${version}" -f "${root}/ansible-${i}.dockerfile" "${root}" || exit 1;
 done;
